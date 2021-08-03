1370d7ffeae01557c5dc38dbb9e83cea25b33681
Sergei Alekseiuk <sdalekseyuk@gmail.com>
2021-08-03 18:37:26 +0300

    moved to folder other

--- other/anti_quicksort.py
+++ other/anti_quicksort.py
@@ -1,11 +1,9 @@
-def anti_quick_sort(input_n):
-    n = input_n
+def anti_quicksort(n):
     lst = [i + 1 for i in range(n)]
     for i in range(2, n):
         lst[i], lst[i // 2] = lst[i // 2], lst[i]
     return lst
 
 
-# Verify it works
-print(anti_quick_sort(5))
-# [1, 4, 5, 3, 2]
+if __name__ == '__main__':
+    print(anti_quicksort(5))  # [1, 4, 5, 3, 2]
