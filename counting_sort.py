6ee70057c41daf0c7f89f5e04926f033390494b1
Sergei Alekseiuk <sdalekseyuk@gmail.com>
2021-08-07 00:58:10 +0300

    renamed lists

--- counting_sort.py
+++ counting_sort.py
@@ -1,23 +1,22 @@
 def counting_sort(lst, sorted_lst, num):
 
-    array = [0 for _ in range(num)]
+    temp = [0 for _ in range(num)]
 
     for i in range(len(lst)):
-        array[lst[i]] += 1
+        temp[lst[i]] += 1
 
     for j in range(1, num):
-        array[j] = array[j] + array[j - 1]
+        temp[j] = temp[j] + temp[j - 1]
 
-    for item in range(len(lst) - 1, -1, -1):
-        sorted_lst[array[lst[item]] - 1] = lst[item]
-        array[lst[item]] -= 1
+    for i in range(len(lst) - 1, -1, -1):
+        sorted_lst[temp[lst[i]] - 1] = lst[i]
+        temp[lst[i]] -= 1
 
     return sorted_lst
 
 
-# Verify it works
-random_list_of_nums = [10, 8, 2, 4, 6, 1, 3, 7, 5]
-list_for_sorting = random_list_of_nums.copy()
-max_number = 11  # max number in list + 1
-result = counting_sort(random_list_of_nums, list_for_sorting, max_number)
-print(result)  # [1, 2, 3, 4, 5, 6, 7, 8, 10]
+if __name__ == '__main__':
+    list_of_nums = [6, 0, 2, 1, 0, 3, 4, 6, 1, 3, 2]
+    sorted_list = [0] * len(list_of_nums)
+    result = counting_sort(list_of_nums, sorted_list, max(list_of_nums) + 1)
+    print(result)  # [0, 0, 1, 1, 2, 2, 3, 3, 4, 6, 6]
